4a43888a992e8670f9d9cbc0b1d293ddca0aaa68
Ronak <upadhyayronak9313@gmail.com>
2020-07-06 20:46:52 +0530

    First CPP file

---
+++ Codeforces/158a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,k;
+	cin>>n>>k;
+	int a[n],cnt=0;
+	for(int i=0;i<n;++i)cin>>a[i];
+	for(int i=0;i<n;++i)
+	{
+		if(a[i]>=a[k-1] && a[i]>0) ++cnt;
+	}
+	cout<<cnt<<"\n";
+	return 0;
+}
